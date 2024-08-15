89a5d29b620d82126c7d7b0b6076aa6e467b1fac
Ömer Tal <omerftal@gmail.com>
2024-08-15 09:49:45 +0300

    Backend operations implemented for My Properties selection processes.

--- RealEstate.Api/Controllers/UserPropertyController.cs
+++ RealEstate.Api/Controllers/UserPropertyController.cs
@@ -53,6 +53,38 @@ namespace RealEstate.Api.Controllers
 
         }
 
+        [HttpGet]
+        [Route("pageSelective")]
+        public IActionResult GetUserPropertyPageSelective(int userId, int page, int pageSize, int propertyTypeId, int propertyStatusId, int currencyId, double lowerBoundMoney, double upperBoundMoney, DateTime startDate, DateTime endDate)
+        {
+            var properties = context.Properties.Include(a => a.PropertyType)
+                                       .Include(a => a.PropertyStatus)
+                                       .Include(a => a.Creator)
+                                       .Include(a => a.Currency)
+                                       .Where(x => !x.IsDeleted && x.CreatorId == userId)
+                                       .AsQueryable();
 
+            if (propertyTypeId != null && propertyTypeId > 0) properties = properties.Where(p => p.PropertyTypeId == propertyTypeId);
+
+            if (propertyStatusId != null && propertyStatusId > 0) properties = properties.Where(p => p.PropertyStatusId == propertyStatusId);
+
+            if (currencyId != null && currencyId > 0) properties = properties.Where(p => p.CurrencyId == currencyId);
+
+            if (lowerBoundMoney != null && lowerBoundMoney > 0) properties = properties.Where(p => p.Price >= lowerBoundMoney);
+
+            if (upperBoundMoney != null && upperBoundMoney > 0) properties = properties.Where(p => p.Price <= upperBoundMoney);
+
+            if (startDate != DateTime.MinValue) properties = properties.Where(p => p.StartDate >= startDate);
+
+            if (endDate != DateTime.MinValue) properties = properties.Where(p => p.EndDate <= endDate);
+
+
+            if (!properties.Any()) return Ok(new { list = properties.ToList(), totalLength = 2 });
+
+            var propertiesList = properties.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            var propertyDtos = propertiesList.Select(PropertyInfoDTO.FromProperty).ToList();
+            return Ok(new { list = propertyDtos, totalLength = propertyDtos.Count });
+        }
     }
 }
